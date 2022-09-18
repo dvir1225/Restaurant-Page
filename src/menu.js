af5dfb556a7a84d9c50822fbdfac9ad7cb5cd24e
Dvir <dvir1225@gmail.com>
2022-09-18 13:14:58 +0300

    add sample dishes

--- src/menu.js
+++ src/menu.js
@@ -28,5 +28,23 @@ export function menu(){
     const fish = createMenuItem('Salmon', 'Delicious fresh caught', 25);
     menu.appendChild(fish)
 
+    const burger = createMenuItem('Burger', 'Grass fed beef', 30);
+    menu.appendChild(burger);
+
+    const pasta = createMenuItem('Pasta', 'Hand made pasta', 25);
+    menu.appendChild(pasta)
+
+    const chips = createMenuItem('French Fries', 'Freshly cut', 15)
+    menu.appendChild(chips);
+
+    const beer = createMenuItem('Malka Admonit', 'Israel\'s finest', 18)
+    menu.appendChild(beer);
+
+    const wine = createMenuItem('Wine selection', 'Ask our waiters', 25)
+    menu.appendChild(wine);
+
+    const softDrink = createMenuItem('Soft Drinks', 'We have it all', 12);
+    menu.appendChild(softDrink);
+
     return menu;
 }
